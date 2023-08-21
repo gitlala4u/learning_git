a75d4adada32ac977ad66b683048f3c486da657a
Rishabh Yadav <you@example.com>
2023-08-21 22:25:14 +0530

    Deleted two lines in cacluater source code

--- calculater.py
+++ calculater.py
@@ -3,17 +3,7 @@ import math
 def add(x, y):
   return x + y
 
-def subtract(x, y):
-  return x - y
 
-def multiply(x, y):
-  return x * y
-
-def divide(x, y):
-  return x / y
-
-def power(x, y):
-  return x ** y
 
 def square_root(x):
   return math.sqrt(x)
